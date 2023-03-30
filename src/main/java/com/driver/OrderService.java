eb3a3e7117770c9b55336a52dc8c37eba10a24b2
unknown <akhileshayush.2016@gmail.com>
2023-03-30 17:40:03 +0530

    Attempt 1

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;import java.util.List;
 @Service
 public class OrderService
 {
-  @Autowired
-  OrderRepository orderRepository ;
+
+  OrderRepository orderRepository =new OrderRepository();
 
     public void addOrder(Order order)
     {
